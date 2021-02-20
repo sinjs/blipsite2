df66c1f3f74cf9f153a614419dc80beb0a13c8c8
sindotexe <git@sinmineryt.ga>
2021-02-20 15:42:42 +0100

    Added Bassboost to the Music commands

--- src/Musiccommands.js
+++ src/Musiccommands.js
@@ -29,6 +29,7 @@ function MusicCommands() {
                 <CommandEntry command="b!shuffle" desc="Shuffles the queue" usage="" />
                 <CommandEntry command="b!remove" desc="Removes a track from the queue" usage="{number}" />
                 <CommandEntry command="b!loop" desc="Loops the current song / queue" usage="{song/queue/song-off/queue-off}" />
+                <CommandEntry command="b!bassboost" desc="Applies bassboost to the song. (rip ears)" usage="{number}" />
                 <CommandEntry command="b!destroyqueue" desc="Resets the queue, use if queue is glitching" usage="" />
             </tbody>
         </Table>
