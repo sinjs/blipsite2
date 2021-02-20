100b29921dfdf74d2a395840018768497d5f1305
sinmineryt <50749166+sinmineryt@users.noreply.github.com>
2021-02-21 01:17:24 +0300

    add alerts for downtime
    directly fetched from sinmineryt.ga/blip-status.json

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './bootstrap.css'
 import React from 'react';
 import Main from './Main';
 import Navbar from './Navbar';
+import Alerts from './Alerts';
 
 function App() {
     return ( 
         <div className="Application" >
             <Navbar />
+            <Alerts />
             <Main />
         </div>
     );
