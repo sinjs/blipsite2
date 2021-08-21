d5d5bd9cd1a3f8d0ff06c9feed50491c9290444b
Aiden Lang <sinmineryt@gmail.com>
2021-08-21 18:30:10 +0000

    Linter: Add `import` to plugins
    Fixes #57 #58

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     "plugin:jest/recommended",
     "plugin:prettier/recommended",
   ],
-  plugins: ["react", "@typescript-eslint", "jest"],
+  plugins: ["react", "@typescript-eslint", "jest", "import"],
   env: {
     browser: true,
     es6: true,
